cbbd8bf2ac7e69ee9934fb8af0df2bb5e54f846c
Dominik <dominikzajda@gmail.com>
2017-11-19 20:40:44 +0100

    Add the FizzBuzz program.

--- SDC/FizzBuzz/Program.cs
+++ SDC/FizzBuzz/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//Write a program that prints the numbers from 1 to 100.
+//But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+//For numbers which are multiples of both three and five print "FizzBuzz".
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var numbers = Enumerable.Range(1, 100).ToList();
+            numbers.ForEach(PrintToConsole);
+
+            Console.ReadLine();
+        }
+
+        private static void PrintToConsole(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+                return;
+            }
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+                return;
+            }
+            if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+                return;
+            }
+            Console.WriteLine(number);
         }
     }
 }
